de1c1252e5dbb9e1aa938c8756b6a5ec99a04a3e
Sayali Lade <ladesayali14@gmail.com>
2025-09-29 15:24:46 +0530

    Add requirements.txt

--- pharmacyproject/settings.py
+++ pharmacyproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w18o+g_=!y_pfauhf@*hya5eib%4@g-q4u(jjbg%2)m4!%nygh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,9 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 import os
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
